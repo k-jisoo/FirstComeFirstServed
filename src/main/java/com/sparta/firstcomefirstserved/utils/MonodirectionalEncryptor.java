e50166636f447cedc9b4324e835ace4124950f04
JISOO\boy74 <boy7442@gmail.com>
2024-09-05 10:51:46 +0900

    에러를 발생시키는 컨트롤러 작성

--- src/main/java/com/sparta/firstcomefirstserved/utils/MonodirectionalEncryptor.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.firstcomefirstserved.utils;
-public interface MonodirectionalEncryptor extends Encryptor{
-
-    @Override
-    public String decrypt(String value) throws Exception;
-
-    @Override
-    public String encrypt(String value) throws Exception;
-}
