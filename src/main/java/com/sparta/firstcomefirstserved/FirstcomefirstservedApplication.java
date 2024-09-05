4ce17c8f33a79345d3833c2ee3c0385401f2b372
JISOO\boy74 <boy7442@gmail.com>
2024-09-05 10:44:25 +0900

    라우팅 설정

--- src/main/java/com/sparta/firstcomefirstserved/FirstcomefirstservedApplication.java
+++ src/main/java/com/sparta/firstcomefirstserved/FirstcomefirstservedApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EntityScan(basePackages = "com.sparta.firstcomefirstserved.user.entity")
+@SpringBootApplication
 public class FirstcomefirstservedApplication {
 
 	public static void main(String[] args) {
